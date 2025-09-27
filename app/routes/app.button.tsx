abbda2d971bc8c30521c02600817940654c2b43b
Md Al Amin Islam <mdalamin212104>
2025-09-27 16:49:09 +0600

    button file added some code

---
+++ app/routes/app.button.tsx
@@ -0,0 +1,53 @@
+import {Page, Badge, CalloutCard} from '@shopify/polaris';
+
+
+export default function PageExample() {
+  return (
+    <Page
+      backAction={{content: 'Products', url: '#'}}
+      title="3/4 inch Leather pet collar"
+      titleMetadata={<Badge tone="success">Paid</Badge>}
+      subtitle="Perfect for any pet"
+      compactTitle
+      primaryAction={{content: 'Save', disabled: true}}
+      secondaryActions={[
+        {
+          content: 'Duplicate',
+          accessibilityLabel: 'Secondary action label',
+          onAction: () => alert('Duplicate action'),
+        },
+        {
+          content: 'View on your store',
+          onAction: () => alert('View on your store action'),
+        },
+      ]}
+      actionGroups={[
+        {
+          title: 'Promote',
+          actions: [
+            {
+              content: 'Share on Facebook',
+              accessibilityLabel: 'Individual action label',
+              onAction: () => alert('Share on Facebook action'),
+            },
+          ],
+        },
+      ]}
+      pagination={{
+        hasPrevious: true,
+        hasNext: true,
+      }}
+    >
+      <CalloutCard 
+        title="Credit card" 
+        illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
+         primaryAction={{
+        content: 'Customize checkout',
+        url: '#',
+      }}
+      >
+        <p>Credit card information</p>
+      </CalloutCard>
+    </Page>
+  );
+}
